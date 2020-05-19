61b76e748dc97b756249da313e1ccc6a19032681
sysophost <sysophost>
2020-05-19 15:44:27 +1000

    added queries

--- modules/queries.py
+++ modules/queries.py
@@ -1,17 +1,23 @@
-def construct_find_date_query(field: str, days: int):
-    data = {
+def construct_find_date_query(flag: str, days: int):
+    if flag == 'logon':
+        field = 'lastlogontimestamp'
+    elif flag == 'password':
+        field = 'pwdlastset'
+
+
+    query = {
         'statements': [{'statement': f'MATCH (u:User) WHERE u.{field} <= (datetime().epochseconds - ({days} * 86400)) and NOT u.{field} IN [-1.0, 0.0] RETURN u ORDER BY u.name ASC'}]
     }
 
-    return data
+    return query
 
 
 def construct_find_object_query(input_objects: list):
-    data = {
+    query = {
         'statements': [{'statement': f'MATCH (n) WHERE n.name IN {input_objects} RETURN n'}]
     }
 
-    return data
+    return query
 
 
 def construct_update_query(input_objects: list, flag: str):
@@ -22,8 +28,8 @@ def construct_update_query(input_objects: list, flag: str):
     elif flag == 'highvalue':
         query_action = 'n.highvalue=TRUE'
 
-    data = {
+    query = {
         'statements': [{'statement': f'MATCH (n) WHERE n.name IN {input_objects} SET {query_action} RETURN n'}]
     }
 
-    return data
+    return query
