c150d2d09be36507df8f3035c3c7c2872a6d9884
sysophost <sysophost>
2020-05-19 15:44:09 +1000

    moved db functions here, added test function

--- modules/db.py
+++ modules/db.py
@@ -1,6 +1,22 @@
 import requests
 
-def query_neo(url: str, headers: str, query: str, timeout: int):
-    neo_response = requests.post(url=url, headers=headers, json=query, timeout=timeout)
 
+def construct_connection(url: str, headers: str, timeout: int):
+    s = requests.Session()
+    s.url = url
+    s.headers = headers
+    s.timeout = timeout
+    return s
+
+def test_connection(session: requests.sessions.Session):
+    try:
+        resp = session.post(session.url)
+        resp.raise_for_status()
+    except Exception as err:
+        raise err
+    else:
+        return True
+
+def query_neo(session: requests.sessions.Session, json: str):
+    neo_response = session.post(session.url, json=json)
     return neo_response
